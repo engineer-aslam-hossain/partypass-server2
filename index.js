a1ce75820700dad5e35a768feba70526f418240c
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2025-03-03 11:29:24 +0200

    trying to use vercel

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! Vercel.");
 });
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
